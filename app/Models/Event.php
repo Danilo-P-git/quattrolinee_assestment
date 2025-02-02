81f611ac1df39110d216a31cc0b414bebb2230ed
Danilo <d.patane98@gmail.com>
2025-02-02 15:55:01 +0100

    Setup relazioni e database con factories

--- app/Models/Event.php
+++ app/Models/Event.php
@@ -25,4 +25,9 @@ class Event extends Model
     use SoftDeletes;
     protected $hidden = ['created_at', 'updated_at', 'deleted_at'];
     protected $fillable = ['name', 'description', 'length', 'start_date'];
+
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
 }
